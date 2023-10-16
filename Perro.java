190fd135ad8c976fcb218b20f29d24cce3d56bdb
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 16:15:58 -0500

    Finalizada clase Perro

--- Perro.java
+++ Perro.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_7_animales;
 
-/**
- *
- * @author Jhony
- */
-public class Perro {
-    
+
+public class Perro extends Canido{
+    public String getSonido(){
+        return "Ladrido";
+    }
+    public String getAlimentos(){
+        return "Carnivoro";
+    }
+    public String getHabitat(){
+        return "Domestico";
+    }
+    public String getNombreCientifico(){
+        return "Canis lupus familiaris";
+    }
 }
