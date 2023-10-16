ac175b3d922dc10df5856fa0e3fcdc8577a21eb8
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 16:11:01 -0500

    Finalizada clase Animal

--- Animal.java
+++ Animal.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_7_animales;
 
-/**
- *
- * @author Jhony
- */
-public class Animal {
+public abstract class Animal {
+    protected String sonido;
+    protected String alimentos;
+    protected String habitat;
+    protected String nombreCientifico;
+    
+    public abstract String getNombreCientifico();
+    public abstract String getSonido();
+    public abstract String getAlimentos();
+    public abstract String getHabitat();
     
 }
