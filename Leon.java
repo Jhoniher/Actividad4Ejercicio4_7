d82562a9f564f3d11ec3bf19fc8a8f176238f906
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 16:23:17 -0500

    Finalizada clase Leon

--- Leon.java
+++ Leon.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_7_animales;
 
-/**
- *
- * @author Jhony
- */
-public class Leon {
-    
+public class Leon extends Felino {
+    public String getSonido(){
+        return "Rugido";
+    }
+    public String getAlimentos(){
+        return "Carnivoro";
+    }
+    public String getHabitat(){
+        return "Praderas";
+    }
+    public String getNombreCientifico(){
+        return "Panthera leo";
+    }
 }
