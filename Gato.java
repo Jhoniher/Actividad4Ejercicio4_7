a3e65614d751f2f7e8935c51b711cf4e5a8db6fd
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 16:26:10 -0500

    Finalizada clase Gato

--- Gato.java
+++ Gato.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_7_animales;
 
-/**
- *
- * @author Jhony
- */
-public class Gato {
-    
+public class Gato extends Felino{
+    public String getSonido(){
+        return "Maullido";
+    }
+    public String getAlimentos(){
+        return "Ratones";
+    }
+    public String getHabitat(){
+        return "Domestico";
+    }
+    public String getNombreCientifico(){
+        return "Felis silvestris catus";
+    }
 }
