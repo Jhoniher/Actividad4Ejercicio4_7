a5df9254f27466189a05aa972b9ed9e8737d1983
Jhony Alpala <jhoniher95@gmail.com>
2023-10-16 16:18:44 -0500

    Finalizada la clase Lobo

--- Lobo.java
+++ Lobo.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ej_4_7_animales;
 
-/**
- *
- * @author Jhony
- */
-public class Lobo {
+public class Lobo extends Canido{
+    public String getSonido(){
+        return "Aullido";
+    }
+    public String getAlimentos(){
+        return "Carnivoro";
+    }
+    public String getHabitat(){
+        return "Bosque";
+    }
+    public String getNombreCientifico(){
+        return "Canis lupues";
+    }
     
 }
